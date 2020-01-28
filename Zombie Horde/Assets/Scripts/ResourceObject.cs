071d3f6f6befbfa821c343e6d12150ee95e43591
Marc <marc@bekhuis.net>
2020-01-28 15:02:13 +0100

    voor remon

---
+++ Zombie Horde/Assets/Scripts/ResourceObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "New Resource", menuName = "Resource")]
+public class ResourceObject : ScriptableObject
+{
+    public Tile[] tiles;
+    public ResourceSystem.ItemGiven[] itemsGiven;
+}
---
+++ Zombie Horde/Assets/Scripts/ResourceSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ResourceSystem : MonoBehaviour
+{
+    public class Resource
+    {
+        ResourceObject resource;
+    }
+
+    [System.Serializable]
+    public class ItemGiven
+    {
+        public Item items;
+        public int amount;
+    }
+
+    [SerializeField] private Tilemap resourceTilemap;
+    [SerializeField] private Tilemap shadowTilemap;
+
+    public void SpawnResource(Vector3 position, ResourceObject resource)
+    {
+
+    }
+}
