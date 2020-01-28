64b8f6987f7057d316049d411593fa84644c5e6b
Anthony Deinum <jinkisaragi@hotmail.com>
2020-01-28 10:06:40 +0100

    KONO DIO DA

---
+++ Zombie Horde/Assets/Scripts/CursorScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    PlayerAiming playerAiming => PlayerAiming.instance;
+    Rigidbody2D rb;
+    Vector3 mousePosition;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = new Vector3(mousePosition.x,mousePosition.y,transform.position.z);
+    }
+}
