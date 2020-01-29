983b8ae053d3f78ce602dfdbaaa30e230a40e83e
Anthony Deinum <jinkisaragi@hotmail.com>
2020-01-29 13:22:21 +0100

    https://www.youtube.com/watch?v=2C3XH3MaEYE

---
+++ Zombie Horde/Assets/FistingScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistingScript : MonoBehaviour
+{
+    PlayerAttack playerAttack => PlayerAttack.instance;
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(playerAttack.playerAnimator.GetBool("isPunching")==true)
+        {
+
+        }
+    }
+}
