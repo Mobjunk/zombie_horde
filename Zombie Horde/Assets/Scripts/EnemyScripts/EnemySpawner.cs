ae2aa7a44d26208f8ff5238761f654c97264f198
Remon <remon@mail.com>
2020-01-29 10:20:57 +0100

    enemy can attack now

--- Zombie Horde/Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Zombie Horde/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemySpawner : MonoBehaviour
 {
@@ -14,14 +15,20 @@ public class EnemySpawner : MonoBehaviour
 
     public void EnemySpawning()
     {
+        Debug.Log(startingAmount + zombiesPerDay * days);
+        //every night it will run this for loop and spawns the amount of zombies. (startingAmount + zombiesPerDay * days)
         for (int i = 0; i < startingAmount + zombiesPerDay * days; i++)
         {   
             GameObject testEnemy = Instantiate(enemy, target.position + Quaternion.Euler(0, 0, Random.Range(0, 360)) * new Vector3(20, 0, 0), Quaternion.identity);
-            if(days < 3){
-                eo[0].SetUp(testEnemy.GetComponent<EnemyMovement>(), target);
+           
+            // if less then 3 days have passed then normal zombies will spawn
+            if(days < 3)
+            {
+                eo[0].SetUp(testEnemy.GetComponent<EnemyMovement>(), testEnemy.GetComponent<EnemyAttack>(), target);
             }
-            if(days > 3){
-                eo[1].SetUp(testEnemy.GetComponent<EnemyMovement>(), target);
+            // if more than 3 days have passed than better zombies will spawn
+            if(days >= 3){
+                eo[1].SetUp(testEnemy.GetComponent<EnemyMovement>(), testEnemy.GetComponent<EnemyAttack>(), target);
             }
         }
     }
