47ecf20754a581200914671075d06fef9e398075
Marc <marc@bekhuis.net>
2020-02-05 12:18:52 +0100

    enemy update (broken)

--- Zombie Horde/Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Zombie Horde/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -12,7 +12,7 @@ public class EnemySpawner : MonoBehaviour
 
     public void EnemySpawning(EnemyObject enemyObject)
     {
-        GameObject testEnemy = Instantiate(enemy, target.position + Quaternion.Euler(0, 0, Random.Range(0, 360)) * new Vector3(20, 0, 0), Quaternion.identity);
-        enemyObject.SetUp(testEnemy.GetComponent<EnemyMovement>(), testEnemy.GetComponent<EnemyHealth>(), target, backgroundTilemap);
+        GameObject spawnedEnemy = Instantiate(enemy, target.position + Quaternion.Euler(0, 0, Random.Range(0, 360)) * new Vector3(20, 0, 0), Quaternion.identity);
+        enemyObject.SetUp(spawnedEnemy.GetComponent<EnemyMovement>(), spawnedEnemy.GetComponent<EnemyHealth>(), target, backgroundTilemap, spawnedEnemy, spawnedEnemy.GetComponentInChildren<EnemyAttack>());
     }
 }
