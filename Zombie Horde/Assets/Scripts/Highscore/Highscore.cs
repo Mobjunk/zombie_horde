2daf868c6c5f7323bc922d083a8a4c1b7e46de10
Mobjunk <34821598+Mobjunk@users.noreply.github.com>
2020-02-11 14:40:43 +0100

    Beginning of highscores & json

---
+++ Zombie Horde/Assets/Scripts/Highscore/Highscore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+ 
+ public class Highscore : JsonHandler<HighscoreEntry>
+ {
+     protected override string GetFileName()
+     {
+         return "highscores.json";
+     }
+ 
+     protected override string GetPath()
+     {
+         return "";
+     }
+
+     public override void Start()
+     {
+         base.Start();
+     }
+ }
---
+++ Zombie Horde/Assets/Scripts/Json/JsonHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public abstract class JsonHandler<T> : MonoBehaviour
+{
+    public List<T> entries = new List<T>();
+
+    protected abstract string GetFileName();
+
+    protected abstract string GetPath();
+
+    public virtual void Start()
+    {
+        Load();
+    }
+
+    public void Save()
+    {
+        var array = new T[entries.Count];
+        
+        for (var index = 0; index < array.Length; index++)
+            array[index] = entries[index];
+        
+        var toJson = JsonHelper.ToJson(array, true);
+        Debug.Log(toJson);
+        
+        var sr = File.CreateText($"{GetPath()}{GetFileName()}");
+        sr.WriteLine (toJson);
+        sr.Close();
+    }
+
+    public void Load()
+    {
+        var jsonString = "";
+        var reader = new StreamReader($"{GetPath()}{GetFileName()}");
+        jsonString = reader.ReadToEnd();
+        reader.Close();
+        
+        var array = JsonHelper.FromJson<T>(jsonString);
+        foreach (var entry in array)
+            entries.Add(entry);
+    }
+}
